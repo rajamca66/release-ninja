54ac7359b213c90555452f1bd09dada54661ce2a
Stephen Bussey <steve.bussey@salesloft.com>
2015-01-18 16:21:25 -0600

    use the slug url publicly

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -3,7 +3,7 @@ class ProjectSerializer < ActiveModel::Serializer
     attributes :id, :full_name, :url
   end
 
-  attributes :id, :created_at, :title, :public_header_background, :public_logo_url, :public_css
+  attributes :id, :created_at, :title, :public_header_background, :public_logo_url, :public_css, :slug
 
   has_many :repositories, each_serializer: RepositorySerializer
 end
