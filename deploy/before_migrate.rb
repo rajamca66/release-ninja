d0e6c38041313bd9520d54b1263fceb9d34596bb
Stephen Bussey <steve.bussey@salesloft.com>
2015-01-26 01:37:03 -0500

    diagnosing weird error with assets failing

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -19,6 +19,6 @@ Chef::Log.info("Precompiling assets for #{rails_env}...")
 
 execute "rake assets:precompile" do
   cwd release_path
-  command "bundle exec rake assets:precompile"
+  command "bundle exec rake assets:precompile && echo 'hi'"
   environment "RAILS_ENV" => rails_env
 end
