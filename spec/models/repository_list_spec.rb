2c07ee1a27e5122d799082b3e0e4e7a0747411c0
Stephen Bussey <steve.bussey@salesloft.com>
2015-04-07 11:13:21 -0400

    make spec be more friendly to a not-set-up testing env

--- spec/models/repository_list_spec.rb
+++ spec/models/repository_list_spec.rb
@@ -9,11 +9,14 @@ RSpec.describe RepositoryList do
   # These are names of your repos that are both private and public. This will
   # be used to verify the code works. It isn't optimal but is easier than stubbing
   # out all HTTP requests
-  let(:your_repos) { YAML.load(File.read( Rails.root.join("spec", "fixtures", "your_repos.private.yaml"))) }
+  let(:your_repos_file) { File.read( Rails.root.join("spec", "fixtures", "your_repos.private.yaml")) }
+  let(:your_repos) { YAML.load(your_repos_file) }
 
-  it "gives visibility to all repos", vcr: { cassette_name: "private/repository-list-1" } do
-    if File.exist?("spec/cassettes/private/repository-list-1.yml") # This is blocking CI because I'm not putting my private data on it
+  if File.exist?("spec/fixtures/your_repos.private.yaml") # This is blocking CI because I'm not putting my private data on it
+    it "gives visibility to all repos", vcr: { cassette_name: "private/repository-list-1" } do
       expect(subject.repositories.map(&:full_name)).to include(*your_repos)
     end
+  else
+    pending "Please enter in a private repo yaml to continue this spec"
   end
 end
