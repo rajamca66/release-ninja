94d136a2d63bd0f7cf185bd99b29adf7fd987d75
Stephen Bussey <steve.bussey@salesloft.com>
2015-04-09 16:18:35 -0400

    incorrect mandrill env...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,8 +87,8 @@ Rails.application.configure do
       :address   => "smtp.mandrillapp.com",
       :port      => 587,
       :enable_starttls_auto => true, # detects and uses STARTTLS
-      :user_name => ENV["MANDRILL_APIKEY"] || ENV["MANDRILL_USERNAME"],
-      :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
+      :user_name => ENV["MANDRILL_USERNAME"],
+      :password  => ENV["MANDRILL_APIKEY"] || ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
       :authentication => 'login', # Mandrill supports 'plain' or 'login'
       :domain => ENV["EMAIL_DOMAIN"]
   }
