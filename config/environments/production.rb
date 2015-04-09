e9ff7f348760bdb2ce3a97842e2e6e497840aab0
Stephen Bussey <steve.bussey@salesloft.com>
2015-04-09 14:21:20 -0400

    use mandrill api key or the mandrill username

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
       :address   => "smtp.mandrillapp.com",
       :port      => 587,
       :enable_starttls_auto => true, # detects and uses STARTTLS
-      :user_name => ENV["MANDRILL_USERNAME"],
+      :user_name => ENV["MANDRILL_APIKEY"] || ENV["MANDRILL_USERNAME"],
       :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
       :authentication => 'login', # Mandrill supports 'plain' or 'login'
       :domain => 'therelease.ninja'
