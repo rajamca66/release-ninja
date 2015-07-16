e2baa6e4201fb73fc63b198c5ba8965ede7ebacb
Justin Barber <barber.justin@gmail.com>
2015-07-16 09:22:00 -0400

    start my specs

---
+++ spec/requests/api/projects/rss_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'Api::Products Rss' do
+
+  context 'when project is valid' do
+    it 'returns 200' do
+      expect(get(api_projects_rss_path(project.id))).to eq 200
+    end
+  end
+end
