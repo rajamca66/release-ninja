fd0a42b7e6d94a965f559c9f0284a25dd2559f9a
Justin Barber <barber.justin@gmail.com>
2015-07-16 11:29:18 -0400

    more specs

--- spec/requests/api/projects/rss_spec.rb
+++ spec/requests/api/projects/rss_spec.rb
@@ -25,5 +25,42 @@ RSpec.describe 'Api::Products Rss' do
     it 'includes link' do
       expect(project_rss).to include "public/#{project.id}</link>"
     end
+    context 'without notes' do
+      it 'does not include items' do
+        expect(project_rss).to_not include "<item>"
+      end
+    end
+    context 'with unpublished notes' do
+      before do
+        project.notes.create(
+          title: 'Fix all the things',
+          markdown_body: 'Fix, fixed, fixedy, fix.',
+          level: :minor
+        )
+      end
+      it 'does not include items' do
+        expect(project_rss).to_not include "<item>"
+      end
+    end
+    context 'with published notes' do
+      before do
+        project.notes.create(
+          title: 'Fix all the things',
+          markdown_body: 'Fix, fixed, fixedy, fix.',
+          level: :minor,
+          published_at: Time.new(2015, 07, 16, 11, 27, 0, -4 * 3600),
+          published: true
+        )
+      end
+      it 'includes items' do
+        expect(project_rss).to include "<item>"
+      end
+      it 'item includes title' do
+        expect(project_rss).to include "<item><title>Fix all the things>/title>"
+      end
+      it 'item includes pubDate' do
+        expect(project_rss).to include "<pubDate>Thu, 16 Jul 2015 11:27:00 -0400<pubDate>"
+      end
+    end
   end
 end
