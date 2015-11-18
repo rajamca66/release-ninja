b8db67bb18cb596d60941a42fd0d8844f85b73f9
Stephen Bussey <steve.bussey@salesloft.com>
2015-11-18 14:00:46 -0500

    authenticate api requests

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,4 +1,11 @@
 class Api::BaseController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  before_filter :authenticate_user!
   respond_to :json
+
+  def authenticate_user!
+    return if current_user
+
+    render json: { error: "You need to sign in or sign up before continuing." }, status: :unauthorized
+  end
 end
