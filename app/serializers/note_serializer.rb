e5d7888692c82d9db2adbb2cab7013ab5eea12d2
Stephen Bussey <steve.bussey@salesloft.com>
2016-04-21 23:39:16 -0400

    serialize a note's filter string

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,6 +1,10 @@
 class NoteSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :title, :markdown_body, :level, :html_title, :html_body,
-             :published, :published_at, :internal
+             :published, :published_at, :internal, :filter_str
+
+  def filter_str # AMS doesn't like you using filter
+    object.filter unless object.published?
+  end
 
   def published_at
     object.published_at.try!(:to_s, :db)
