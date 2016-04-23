4f9ddec7585b032841872470d170d1726d29890e
Stephen Bussey <steve.bussey@salesloft.com>
2016-04-23 13:59:23 -0400

    update UserReadingLocation for a user_key if given on published_notes fetch

--- app/controllers/site/published_notes_controller.rb
+++ app/controllers/site/published_notes_controller.rb
@@ -2,7 +2,8 @@ class Site::PublishedNotesController < Site::BaseController
   PER_PAGE = 10
 
   def index
-    respond_with :site, published_notes.page(page).per(PER_PAGE), each_serializer: SiteApi::NoteSerializer
+    update_user_reading_location(paged_notes.first) if user_key
+    respond_with :site, paged_notes, each_serializer: SiteApi::NoteSerializer
   end
 
   private
@@ -13,5 +14,26 @@ class Site::PublishedNotesController < Site::BaseController
 
   def published_notes
     project.notes.published_filtered.order(published_at: :desc, id: :desc)
+  end
+
+  def paged_notes
+    @paged_notes ||= published_notes.page(page).per(PER_PAGE).to_a
+  end
+
+  def user_key
+    params[:user_key]
+  end
+
+  def update_user_reading_location(note)
+    new_location = note.published_at || Time.current
+
+    if user_reading_location.reading_location.blank? || new_location > user_reading_location.reading_location
+      user_reading_location.reading_location = new_location
+      user_reading_location.save
+    end
+  end
+
+  def user_reading_location
+    @user_reading_location ||= project.user_reading_locations.where(user_key: user_key).first_or_initialize
   end
 end
--- spec/controllers/site/published_notes_controller_spec.rb
+++ spec/controllers/site/published_notes_controller_spec.rb
@@ -7,18 +7,20 @@ RSpec.describe Site::PublishedNotesController, type: :controller do
     let!(:published_notes) { FactoryGirl.create_list(:note, 3, project: project, published: true) }
     let!(:unpublished_notes) { FactoryGirl.create_list(:note, 4, project: project) }
 
+    subject(:make_request!) { get :index, site_id: project.id }
+
     it "is successful" do
-      get :index, site_id: project.id
+      make_request!
       expect(response).to be_success
     end
 
     it "returns published notes" do
-      get :index, site_id: project.id
+      make_request!
       expect(response_json.length).to eq(3)
     end
 
     it "returns desired fields" do
-      get :index, site_id: project.id
+      make_request!
       expect(response_json[0].keys).to match_array(["id", "published_at", "html_title", "html_body"])
     end
 
@@ -26,7 +28,7 @@ RSpec.describe Site::PublishedNotesController, type: :controller do
       let!(:published_notes) { FactoryGirl.create_list(:note, 30, project: project, published: true) }
 
       it "pages the request" do
-        get :index, site_id: project.id
+        make_request!
         expect(response_json.length).to eq(10)
         expect(response_json[0]["id"]).to eq(published_notes.last.id)
       end
@@ -35,6 +37,45 @@ RSpec.describe Site::PublishedNotesController, type: :controller do
         get :index, site_id: project.id, page: 2
         expect(response_json.length).to eq(10)
         expect(response_json[0]["id"]).to eq(published_notes[19].id)
+      end
+    end
+
+    context "with a user_key" do
+      subject(:make_request!) { get :index, site_id: project.id, user_key: "steve" }
+
+      it "creates a new UserReadingLocation" do
+        expect {
+          make_request!
+        }.to change { project.user_reading_locations.count }.by(1)
+      end
+
+      it "uses an existing UserReadingLocation" do
+        project.user_reading_locations.create!(user_key: "steve", reading_location: Time.current)
+        expect {
+          make_request!
+        }.not_to change { project.user_reading_locations.count }
+      end
+
+      it "sets reading_location" do
+        make_request!
+        expect(UserReadingLocation.last.reading_location).to eq(published_notes.last.published_at)
+      end
+
+      it "uses the current  time if published_at is nil" do
+        Note.update_all(published_at: nil)
+        make_request!
+        expect(UserReadingLocation.last.reading_location).to be_within(2).of(Time.current)
+      end
+
+      context "with a reading_location later than the published_at" do
+        let!(:time) { 5.days.from_now }
+        let!(:reading_location) { project.user_reading_locations.create!(user_key: "steve", reading_location: time) }
+
+        it "doesn't update the reading_location" do
+          expect {
+            make_request!
+          }.not_to change { reading_location.reload.attributes }
+        end
       end
     end
   end
