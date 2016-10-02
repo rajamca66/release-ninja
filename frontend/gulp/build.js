bb8c20c47463d6e0768c4b08d88842a5e7827808
Stephen Bussey <steve.bussey@salesloft.com>
2016-10-02 00:10:11 -0400

    better build to the rails app

--- frontend/gulp/build.js
+++ frontend/gulp/build.js
@@ -27,3 +27,17 @@ gulp.task('clean', function () {
 });
 
 gulp.task('build', ['scripts', 'styles', 'fonts']);
+
+gulp.task('build:production', ['build'], function() {
+  return gulp.start("files:move");
+});
+
+gulp.task('files:move', function() {
+  return $.shell.task([
+    'mkdir -p ../public/scripts ../public/styles ../public/fonts',
+    'rm -f ../public/scripts/* ./backend/public/styles/* ./backend/public/fonts/*',
+    'cp dist/scripts/* ../public/scripts/',
+    'cp dist/styles/* ../public/styles/',
+    'cp dist/fonts/* ../public/fonts'
+  ]);
+});
