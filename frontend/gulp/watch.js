ba2a7cfe77de53c59df9be3a7c5a8e777e711a90
Stephen Bussey <sb8244@cs.ship.edu>
2016-10-08 12:07:16 -0700

    clean up to prevent git-cruft build up

--- frontend/gulp/watch.js
+++ frontend/gulp/watch.js
@@ -4,18 +4,29 @@ var path = require('path');
 var gulp = require('gulp');
 var conf = require('./conf');
 var runSequence = require('run-sequence').use(gulp);
+var $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'del']
+});
+
+gulp.task('watch:clean:styles', function () {
+  return $.del(path.join(conf.paths.dist, '/styles'));
+});
+
+gulp.task('watch:clean:scripts', function () {
+  return $.del(path.join(conf.paths.dist, '/scripts'));
+});
 
 gulp.task('watch', ['build:development'], function () {
   gulp.watch([
     path.join(conf.paths.src, '/stylesheets/**/*.scss')
   ], function() {
-    runSequence('styles', 'revision', 'files:move');
+    runSequence('watch:clean:styles', 'styles', 'revision', 'files:move');
   });
 
   gulp.watch([
     path.join(conf.paths.src, '/**/*.html.slim'),
     path.join(conf.paths.src, '/**/*.js')
   ], function() {
-    runSequence('scripts', 'revision', 'files:move')
+    runSequence('watch:clean:scripts', 'scripts', 'revision', 'files:move')
   });
 });
