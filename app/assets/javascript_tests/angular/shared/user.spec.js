d28325a64d9ab3643887af62e47d4a0ba3165678
Stephen Bussey <sb8244@cs.ship.edu>
2016-10-09 15:20:48 -0400

    es6 spec that would fail in phantom if not compiled

--- app/assets/javascript_tests/angular/shared/user.spec.js
+++ app/assets/javascript_tests/angular/shared/user.spec.js
@@ -2,30 +2,30 @@ describe("User", function() {
   var User,
       $window;
 
-  beforeEach(inject(function(_User_, _$window_) {
+  beforeEach(inject((_User_, _$window_) => {
     User = _User_;
     $window = _$window_;
   }));
 
   describe("getUser", function() {
-    it("returns $window.current_user", function() {
+    it("returns $window.current_user", () => {
       $window.current_user = "test";
       expect(User.getUser()).toEqual("test");
     });
 
-    it("works without $window.current_user set", function() {
+    it("works without $window.current_user set", () => {
       delete $window.current_user;
       expect(User.getUser()).not.toBeDefined();
     });
   });
 
   describe("isLoggedIn", function() {
-    it("returns true when $window.current_user", function() {
+    it("returns true when $window.current_user", () => {
       $window.current_user = "test";
       expect(User.isLoggedIn()).toEqual(true);
     });
 
-    it("works without $window.current_user set", function() {
+    it("works without $window.current_user set", () => {
       delete $window.current_user;
       expect(User.isLoggedIn()).toEqual(false);
     });
